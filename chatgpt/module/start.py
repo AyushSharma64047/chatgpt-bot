aa2a6c90a57b45c6eec74f7a37d0623df6d22f43
Ayush Sharma <106085120+AyushSharma64047@users.noreply.github.com>
2023-04-15 17:46:54 +0530

    Update start.py

--- chatgpt/module/start.py
+++ chatgpt/module/start.py
@@ -7,13 +7,34 @@
 # All rights reserved. See COPYING, AUTHORS.
 #
 
-from pyrogram import Client as ren
+import requests
+import os
+import json
+import random
+import asyncio
 from pyrogram import *
 from pyrogram.types import *
+from pyrogram import Client as ren 
+from pyrogram.errors import MessageNotModified
+from chatgpt.module.what import *
+from config import OPENAI_API 
 
-@ren.on_message(filters.command("start") & filters.private)
-async def start_bot(c: Client, m: Message):
-    start_welcome = f"Hey What's Up! {m.from_user.mention}\n\nWelcome to GPT-3.5-turbo AI! Send me any message (via /ask command ) and I will use the Open A.I API to generate the response & solve the query for you.\n\nExample `/ask hello what's your version?`"
-    start_button = InlineKeyboardMarkup([[InlineKeyboardButton("➕ADD ME to Ur Group➕", url=f"https://t.me/{c.me.username}?startgroup=True")]])
-    start_button = InlineKeyboardMarkup([[InlineKeyboardButton("🔖Bot Lists🔖", url=f"https://t.me/mdisk_bots")
-await m.reply_text(start_welcome, reply_markup=start_button)
+CMD_HANDLER = ["!", "/"] # your change handler
+
+cmd = CMD_HANDLER
+
+@ren.on_message(filters.command(["ai", "ask"], cmd) & filters.private | filters.group)
+async def chatgpt(c: Client, m: Message):
+    randydev = (m.text.split(None, 1)[1] if len(m.command) != 1 else None)
+    if not randydev:
+       await m.reply(f"use command <code>/{m.command[0]} [question]</code> to ask questions using the API.")
+       return
+    headers = {"Content-Type": "application/json", "Authorization": f"Bearer {OPENAI_API}"}
+    json_data = {"model": "gpt-3.5-turbo", "messages": [{"role": "user", "content": randydev}]}
+    try:
+        response = (await http.post("https://api.openai.com/v1/chat/completions", headers=headers, json=json_data)).json()
+        await c.send_chat_action(m.chat.id, enums.ChatAction.TYPING)
+        await asyncio.sleep(2)
+        await c.send_message(m.chat.id, response["choices"][0]["message"]["content"], reply_to_message_id=m.id)
+    except Exception:
+        await c.send_message(m.chat.id, "Yahh, sorry i can't get your answer.", reply_to_message_id=m.id)
